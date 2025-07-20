db1aafb6154a6d1a485280c362963e5c93488575
Tushar <td220627@gmail.com>
2025-07-20 13:05:14 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.cnnClassifier import logger
+from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
